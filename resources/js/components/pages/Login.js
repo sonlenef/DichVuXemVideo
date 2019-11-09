4f41e54cca232db5b81193976ae518017437dc1b
Le Son <leson@Les-MacBook-Pro.local>
2019-11-09 23:53:14 +0700

    UI Login

--- resources/js/components/pages/Login.js
+++ resources/js/components/pages/Login.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 class Login extends Component {
     render() {
-        return (<div>
-            <h1>Welcome to Login!</h1>
-        </div>)
+        return (
+            <form className="boxLogin">
+                <h1>Login</h1>
+                <input type="text" name="" placeholder="Username" required/>
+                <input type="password" name="" placeholder="Password" required/>
+                <input type="submit" name="" placeholder="Login"/>
+            </form>
+        )
     }
 }
 
